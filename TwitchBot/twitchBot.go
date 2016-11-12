bba7fc5a5baf0bea9afe2e966181954b9061d461
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-12 08:03:25 -0500

    now closes the connection

--- TwitchBot/twitchBot.go
+++ TwitchBot/twitchBot.go
@@ -24,7 +24,7 @@ func main(){
 func runBot() (err error){
 
 	ircConnection, err := irc.CreateIrcConnection(&botIrcInfo)
-
+	defer ircConnection.CloseConnection()
 	api.SetIrcConnection(ircConnection)
 	go api.Ini()
 	if err != nil{
