47c092fd85d460915b84d6a93afbdceb474311fa
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-12 07:22:00 -0500

    Initial commit

---
+++ TwitchBot/twitchBot.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"./irc"
+	"./api"
+)
+
+
+
+var botIrcInfo = irc.IrcConnectionInfo{"irc.twitch.tv", 6667, "frde_bot", "oauth:qmsz9bc54rqc5429r05oomjsvhbzkm"}
+
+
+func main(){
+
+
+	err := runBot();
+	if err != nil{
+		fmt.Println(err.Error())
+	}
+}
+
+
+func runBot() (err error){
+
+	ircConnection, err := irc.CreateIrcConnection(&botIrcInfo)
+
+	api.SetIrcConnection(ircConnection)
+	go api.Ini()
+	if err != nil{
+		return err
+	}
+
+	ircConnection.Authenticate()
+
+	ircConnection.JoinChannel("winter_squirrel")
+
+	for{
+
+		line, err := ircConnection.GetMessage()
+
+		if err != nil{
+			return err
+		}
+
+		fmt.Println(line)
+	}
+
+	return nil
+}
+
+
