628023c26af9b0db0a6c3b88c0f30143c3d78512
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-11-13 08:28:05 -0500

    Added proxy handler for twitch bot

--- Website/frde.go
+++ Website/frde.go
@@ -4,6 +4,10 @@ import (
 	"net/http"
 	"html/template"
 	"encoding/json"
+	"time"
+	"fmt"
+	"io/ioutil"
+	"log"
 )
 
 var WebsitePath = "Website/"
@@ -20,9 +24,13 @@ type channel struct{
 }
 
 
-
+//Get the json from a url and map it to an interface
 func getJson(url string, target interface{}) error{
-	r, err := http.Get(url)
+	//timout after 5 seconds
+	timeout := time.Duration(5 * time.Second)
+	client := http.Client{Timeout: timeout}
+
+	r, err := client.Get(url)
 	if err != nil{
 		return err;
 	}
@@ -33,15 +41,42 @@ func getJson(url string, target interface{}) error{
 
 func adminHandler(w http.ResponseWriter, r *http.Request){
 	var channels []channel
-	getJson("http://localhost:8081/channels/", &channels)
-	templates.ExecuteTemplate(w, "admin.html", channels)
+	if err := getJson("http://localhost:8081/channels/", &channels); err != nil{
 
+		return
+	}
+	templates.ExecuteTemplate(w, "admin.html", channels)
+}
+
+//This handler is a proxy between the website and the twitch bot\
+// a user can specify a channel  (ex: winter_squirrel) and an action (ex: leave / join)
+// And the request will be forwarded to the twitch bot
+func channelHandler(w http.ResponseWriter, r *http.Request) {
+	client := http.Client{Timeout: time.Duration(5 * time.Second)}
+
+	//Get channel and action from body
+	channel := r.FormValue("channel")
+	action := r.FormValue("action")
+
+	//TODO add other values to proxy url so complex commands can also be called
+
+	//Ask the twitch bot to do the action
+	response, err := client.Get(fmt.Sprintf("http://localhost:8081/channel/%s/%s", channel, action))
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+	defer response.Body.Close()
+	//Give back the twitch bot response
+	body, err := ioutil.ReadAll(response.Body)
+	w.Write([]byte(body))
+	return
 }
 
 
-
 func main(){
-	http.HandleFunc("/admin/", adminHandler)
+	//TODO add authentication for admin subpath
+	http.HandleFunc("/admin/panel/", adminHandler)
+	http.HandleFunc("/admin/channel/", channelHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
